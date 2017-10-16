9ba6cd06dc2f857514a9536c5920d31abc5e69e4
Pasi Hirvonen <pasi.hirvonen@octo3.fi>
2017-10-16 08:22:58 +0300

    Initial import

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+function getPosts() {
+  const getPosts = axios.get("https://jsonplaceholder.typicode.com/posts");
+  const getUsers = axios.get("https://jsonplaceholder.typicode.com/users");
+
+  const getBoth = Promise.all([getPosts, getUsers]);
+
+  return getBoth.then(function(response) {
+    const posts = response[0].data;
+    const users = response[1].data;
+
+    return posts.map(function addUserData(post) {
+      return Object.assign({}, post, {
+        userData: users.find(function findUser(user) {
+          return user.id === post.userId;
+        })
+      });
+    });
+  });
+}
+
+function getCommentsForPost(postId) {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/posts/" + postId + "/comments")
+    .then(function(response) {
+      return response.data;
+    });
+}
+
+module.exports = {
+  getPosts,
+  getCommentsForPost
+};
