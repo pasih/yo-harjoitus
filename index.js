0cfafbc39443d59ab9a4fdc1c8606577c5f56b81
Pasi Hirvonen <pasi.hirvonen@visma.com>
2020-10-12 15:05:52 +0300

    Update

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const axios = require("axios");
 
-function getPosts() {
+function getPosts(giveMeError) {
   const getPosts = axios.get("https://jsonplaceholder.typicode.com/posts");
   const getUsers = axios.get("https://jsonplaceholder.typicode.com/users");
 
   const getBoth = Promise.all([getPosts, getUsers]);
 
-  return getBoth.then(function(response) {
+  return getBoth.then(function (response) {
     const posts = response[0].data;
     const users = response[1].data;
+
+    if (giveMeError) {
+      throw new Error("Oopsie");
+    }
 
     return posts.map(function addUserData(post) {
       return Object.assign({}, post, {
         userData: users.find(function findUser(user) {
           return user.id === post.userId;
-        })
+        }),
       });
     });
   });
@@ -23,12 +27,12 @@ function getPosts() {
 function getCommentsForPost(postId) {
   return axios
     .get("https://jsonplaceholder.typicode.com/comments?postId=" + postId)
-    .then(function(response) {
+    .then(function (response) {
       return response.data;
     });
 }
 
 module.exports = {
   getPosts,
-  getCommentsForPost
+  getCommentsForPost,
 };
